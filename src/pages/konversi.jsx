ac736e96cb5d3b480a3ebcb3d15f08630b013f9f
Akhmad Fahrizal <105226689+afahrizal@users.noreply.github.com>
2023-10-15 19:47:47 +0700

    Update konversi.jsx

--- src/pages/konversi.jsx
+++ src/pages/konversi.jsx
@@ -27,7 +27,7 @@ const Konversi = () => {
         <p className="text-xl font-bold mb-4 text-center">Konversi Mata Uang dari IDR ke USD</p>
         <div className="justify-center flex item-center">
           <input className="p-2 border-2 border-blue-600 rounded-lg mx-4 h-min" type="text" value={idr} onChange={(event) => {setIdr(event.target.value)}}/>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-5 drop-shadow-lg" onClick={fetchData} disabled={loading}>Get Facts</button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-5 drop-shadow-lg" onClick={fetchData} disabled={loading}>Konversi</button>
         </div>
         <p className="text-2xl font-serif mb-4">$ {data.new_amount}</p>
       </div>
